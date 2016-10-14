5b2f5db9a1e91cffdb504a317223f421bf03d657
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-14 15:39:09 +0800

    Step 2. create a second view and use segue to navigatte to the view. you could go back and forth in these views. close project

---
+++ NavigationView/newViewController.swift
@@ -0,0 +1,21 @@
+//
+//  newViewController.swift
+//  NavigationView
+//
+//  Created by Sgmedical on 2016/10/14.
+//  Copyright © 2016年 Sgmedical. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class newViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
